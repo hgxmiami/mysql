39edf5a14606f718c3cc22c0fb0352b5c8d237a0
kikilin1 <kikiqianlin@gmail.com>
2015-11-18 20:40:02 -0500

    changes day 6
    day 6

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ var connection = mysql.createConnection({
 var port= 5235;
 var express= require('express');
 var app= express();
+var bodyParser = require("body-parser");
 
 app.use(function(req, res, next){
         res.header("Access-Control-Allow-Origin", "*");
         next();
 });
+app.use(bodyParser.urlencoded( {extended: true} )  );
 
 connection.connect();
 app.get('/transactions', function(req, res){
@@ -30,6 +32,24 @@ app.get('/transactions', function(req, res){
         }
     });
 });
+
+
+app.post('/transactions/:sid/:donation', function(req, res){
+    var sid = req.params.sid;
+    var donation = req.params.donation;
+    connection.query("INSERT INTO transactions (sid, donation) VALUES"+   
+                 "   ("+sid+", "+donations+")", function(error){
+        if(error){
+              console.log("DB insertion failed: "+error);
+              res.end("DB error");
+        }
+        else{
+              console.log("DB insertion succeeded");
+              res.end("Success");
+        }
+    });
+});
+
 
 app.listen(port);
 console.log("Server listening on port "+port);
